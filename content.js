d336c5660322e5dae6897a43d1eef112541f7c93
Jiaqi Wang <164467952+jqwgt@users.noreply.github.com>
2025-02-10 18:02:21 +0800

    Delete content.js

--- content.js
+++
@@ -1,9 +0,0 @@
-console.log("content.js 开始执行");
-
-chrome.runtime.sendMessage({ action: "testMessage", message: "Hello from content.js" }, () => {
-    if (chrome.runtime.lastError) {
-        console.error("发送消息失败:", chrome.runtime.lastError);
-    } else {
-        console.log("成功发送测试消息");
-    }
-});
